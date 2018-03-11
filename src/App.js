736d286ec15f62a3c98bd7997bd78b436601a40e
Hein Lin Aung <heinlinaung6891@gmail.com>
2018-03-12 02:05:08 +0630

    Added firebase module and integrated firebase config in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+	componentWillMount(){
+		firebase.initializeApp({
+	    apiKey: "AIzaSyCg7-3452hGfAzfevwzbbB9jB0poiQu3kQ",
+	    authDomain: "react-native-auth-app-3490c.firebaseapp.com",
+	    databaseURL: "https://react-native-auth-app-3490c.firebaseio.com",
+	    projectId: "react-native-auth-app-3490c",
+	    storageBucket: "react-native-auth-app-3490c.appspot.com",
+	    messagingSenderId: "775566630890"
+  	})
+	}
+
 	render() {
 		return(
 			<View>
